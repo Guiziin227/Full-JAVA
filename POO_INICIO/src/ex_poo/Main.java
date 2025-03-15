a8d65de6b2d4767f0926e95b0327a11b74ab4eaa
Guilherme Weber <gh63388@gmail.com>
2025-03-15 15:42:12 -0300

    implement triangle area calculation using OOP

---
+++ POO_INICIO/src/ex_poo/Main.java
@@ -0,0 +1,43 @@
+package ex_poo;
+
+import ex_poo.entities.Triangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public  static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("With POO");
+        System.out.println("Entre com as medidas do triângulo X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Entre com as medidas do triângulo Y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double areaX = x.area();
+        double areaY = y.area();
+
+        System.out.printf("Área do triângulo X: %.4f%n", areaX);
+        System.out.printf("Área do triângulo Y: %.4f%n", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Maior área: X");
+        } else if(areaY > areaX) {
+            System.out.println("Maior área: Y");
+        } else {
+            System.out.println("Áreas iguais");
+        }
+
+
+        sc.close();
+    }
+}
