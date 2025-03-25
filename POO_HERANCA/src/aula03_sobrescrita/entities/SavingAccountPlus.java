e0ea4eefa275b1510bfd61b24bda20fa1daa2ba1
Guilherme Weber <gh63388@gmail.com>
2025-03-25 14:32:39 -0300

    add SavingAccountPlus class with modified withdraw method and finalize withdraw in SavingsAccount

---
+++ POO_HERANCA/src/aula03_sobrescrita/entities/SavingAccountPlus.java
@@ -0,0 +1,8 @@
+package aula03_sobrescrita.entities;
+
+public class SavingAccountPlus extends SavingsAccount{
+    @Override
+    public void withdraw(double amount) {
+        balance -= amount + 2.0;
+    }
+}
