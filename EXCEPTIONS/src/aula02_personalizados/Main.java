847794425fe24c732ac9ab63f8ef6202e4a873a6
Guilherme Weber <gh63388@gmail.com>
2025-03-29 17:51:03 -0300

    add date handling and reservation update logic in Main class (metodo medio)

--- EXCEPTIONS/src/aula02_personalizados/Main.java
+++ EXCEPTIONS/src/aula02_personalizados/Main.java
@@ -32,17 +32,16 @@ public class Main {
             System.out.println("Check-out date (dd/MM/yyyy): ");
             checkout = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if (chekin.before(now) || checkout.before(now)) {
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            } else if (!checkout.after(chekin)) {
-                System.out.println("Error in reservation: Check-out date must be after check-in date");
+
+            String error = reservation.updateDates(chekin, checkout);
+
+            if (error != null) {
+                System.out.println(error);
             } else {
-                reservation.updateDates(chekin, checkout);
                 System.out.println("Reservation: " + reservation);
             }
 
         }
-            sc.close();
-        }
+        sc.close();
     }
+}
