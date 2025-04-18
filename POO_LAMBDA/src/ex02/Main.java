1dc66137866969207f4d6bed510101085c945cd5
Guiziin227 <gh63388@gmail.com>
2025-04-18 11:17:15 -0300

    add Person class and main method to read data from file and filter names by salary

---
+++ POO_LAMBDA/src/ex02/Main.java
@@ -0,0 +1,38 @@
+package ex02;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        String path = "./POO_LAMBDA/src/ex02/in.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            List<Person> people = new ArrayList<>();
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+                people.add(new Person(fields[0], fields[1], Double.parseDouble(fields[2])));
+            }
+
+            List<String> names = people.stream()
+                    .filter(p -> p.getSalary() < 2000.0)
+                    .map(Person::getName)
+                    .collect(Collectors.toList());
+
+            for (String p : names) {
+                System.out.println(p);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
